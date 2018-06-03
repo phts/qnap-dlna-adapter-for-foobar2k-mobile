39a54d2d1794cd072050149451f9e13379b4d5bf
Phil Tsarik <phil.tsarik@gmail.com>
2018-06-03 20:35:29 +0200

    Add skeleton

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 #!/usr/bin/env node
+
+const express = require('express')
+const app = express()
+
+const HOST = '0.0.0.0'
+const PORT = 8782
+
+app.get('/DeviceDescription.xml', (req, res) => {
+  // TODO redirect to /rootDesc.xml
+  console.log('get /DeviceDescription.xml', req)
+  res.status(500).send('get /DeviceDescription.xml')
+})
+
+app.get('*', (req, res) => {
+  console.log('get *', req)
+  res.status(500).send('get *')
+})
+
+app.post('*', (req, res) => {
+  console.log('post *', req)
+  res.status(500).send('post *')
+})
+
+app.listen(PORT, HOST, () => {
+  console.info(`Listening on ${HOST}:${PORT}.`)
+})
